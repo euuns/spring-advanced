428399953b913022d3514389342b83a7b5fdbbcb
euuns <dmsu2@naver.com>
2025-02-26 16:42:59 +0900

    Feat(Lv4): AOP를 활용한 LOGGING 구현
    point cut: * org.example.expert.domain.*.*.*AdminController.*(..))
    domain아래 user와 commnet패키지 하위에 있는 controller패키지를 적용
    해당 패키지에서 클래스 이름에 'AdminController'가 포함된 클래스 적용
    그 안의 모든 메서드, 모든 파라미터에 적용
    -
    실행을 요청한 method는 [패키지.클래스.메서드]형태로 나오도록 지정
    -
    요청한 user정보는 Filter에서 setAttribute로 넣어준 [userId]를 그대로 사용
    HttpServletRequest.getAttribute(userId)로 가져와 작성
    -
    요청 시각은 LocalTime.now(), 요청 url은 HttpServletRequest.getRequestURL()
    -
    각 요청본문과 응답본문을 JSON형식으로 기록하기 위해 ObjectMapper 사용

---
+++ src/main/java/org/example/expert/config/LoggingAspect.java
@@ -0,0 +1,43 @@
+package org.example.expert.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Aspect
+@Slf4j
+@Component
+public class LoggingAspect {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+
+    @Around("execution(* org.example.expert.domain.*.*.*AdminController.*(..))")
+    public void adminLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+
+        log.info("method = {}.{}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+
+        log.info("user = {}", request.getAttribute("userId"));
+        log.info("url = {}", LocalDateTime.now());
+        log.info("time = {}", request.getRequestURL().toString());
+
+        String request = mapper.writeValueAsString(args);
+        log.info("request = {}", request);
+
+        String response = mapper.writeValueAsString(joinPoint.proceed());
+        log.info("response = {}", response);
+    }
+
+
+}
